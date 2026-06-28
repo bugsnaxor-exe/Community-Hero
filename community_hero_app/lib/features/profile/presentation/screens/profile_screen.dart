a40ef3243d4eea0e6b5812bf924126259f2a2bde
Sayantan Dasgupta <sayantan05092004@gmail.com>
2026-06-28 20:20:30 +0530

    fix: correct import path for authControllerProvider in profile_screen

--- community_hero_app/lib/features/profile/presentation/screens/profile_screen.dart
+++ community_hero_app/lib/features/profile/presentation/screens/profile_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import '../../auth/presentation/providers/auth_controller.dart';
+import '../../../auth/presentation/providers/auth_controller.dart';
 import '../providers/profile_controller.dart';
 import '../../../../models/user.dart';
 
